45de0f3ad50762a77b035584871b44462949abc8
javipus <javier.prietop@estudiante.uam.es>
2020-12-18 15:30:54 +0100

    Alternative definition of quantum_state proposed

--- src/state.lean
+++ src/state.lean
@@ -13,12 +13,17 @@ only non-negative eigenvalues.
 -/
 class quantum_state (ρ : module.End ℂ ℋ) :=
 (trace_one : Tr ρ = 1)
--- current definition says all real eigenvalues of ρ are positive, but positive semidefiniteness is stronger: all eigenvalues are real AND positive
--- (pos_semidef : ∀ e : ℂ, ρ.has_eigenvalue e → (e ∈ ℝ) ∧ (e > 0))
 (pos_semidef : ∀ e : ℝ, ρ.has_eigenvalue e → e ≥ 0)
+-- Current definition says all real eigenvalues of ρ are positive, but density matrices satisfy a stronger requirement: all eigenvalues are real AND positive
+-- (pos_semidef : ∀ e : ℂ, ρ.has_eigenvalue e → (e ∈ ℝ) ∧ (e > 0))
+-- Alternatively, we could define this more abstractly
+-- (hermitian : ∀ x y ∈ ℋ, inner x (ρ y) = inner (ρ x) y)
+-- (pos_semidef : ∀ x : ℋ, inner x (ρ x) ≥ 0)
 
 -- I want to declare quamtum states without the wordy `ρ : module.End ℂ H [quantum_state ρ]` but I'm not sure this is the right type definition
 abbreviation State (ℋ : Type) [complex_hilbert_space ℋ] := ∀ ρ : module.End ℂ ℋ, quantum_state ρ
+
+def spectrum (ρ : module.End ℂ ℋ) [quantum_state ρ] : list ℝ := sorry
 
 universe x
 
