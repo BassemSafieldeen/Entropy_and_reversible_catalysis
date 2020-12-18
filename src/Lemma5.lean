7bc5a1f9e938a0057c47f2626eb96d51feb74c23
javipus <javier.prietop@estudiante.uam.es>
2020-12-18 15:29:32 +0100

    Fixed notation

--- src/Lemma5.lean
+++ src/Lemma5.lean
@@ -1,17 +1,36 @@
-def V := ∑ i, (V i) ⊗ |i⟩⟨i|
+import to_mathlib_maybe.Hilbert_space
+import to_mathlib_maybe.pTrace
+import rnd_var
+import state
 
-def σ := ∑ i, (p i) * |i⟩⟨i|
+variables
+{ι : Type} [fintype ι] [decidable_eq ι]
+{ℋ : Type} [complex_hilbert_space ℋ]
+{ρ : module.End ℂ ℋ} [quantum_state ρ]
+{n m d : ℕ} -- dim S, S' and R
+{V : ι → unitary_group ℋ}
+{p : ι → ℝ} [rnd_var p]
 
-def C (ρ) = Tr₂ (V ∘ (ρ ⊗ σ) ∘ V†)
+open_locale tensor_product big_operators
+
+def E (i j : ι) : module.End ℂ ℋ := sorry -- d×d matrix with 1 at (i j) and 0 otherwise
+notation `|`i`⟩⟨`j`|` := E i j
+
+def V' := ∑ i : ι, (V i) ⊗ |i⟩⟨i|
+
+def σ := ∑ i : ι, (p i) • |i⟩⟨i|
+
+def C (ρ) := Tr₂ (V' ∘ (ρ ⊗ σ) ∘ V†)
 
 -- def C (ρ) : ∑ i, (p i) * (V i) ∘ ρ ∘ (V i)† -- double check this
 
-
+-- TODO partial trace is undefined for entangled states
 lemma correlations_dont_propagate : 
-Tr₁ ((V ⊗ 1) ρ_SS' ⊗ σ (V ⊗ 1)†) = ρ_S' ⊗ σ := 
+Tr₁ ((V ⊗ Id (dim σ)) ρ ⊗ σ (V ⊗ Id (dim σ))†) = Tr₁(ρ) ⊗ σ := 
 begin
     -- nice, the proof is short.
-    calc Tr₁ ((V ⊗ 1) ρ_SS' ⊗ σ (V ⊗ 1)†) = ∑ i, (p i) * Tr₁ ((V ⊗ 1) ρ_SS' (V ⊗ 1)†) ⊗ |i⟩⟨i| : by sorry,
-... = ∑ i, (p i) * ρ_S' ⊗ |i⟩⟨i| : by sorry,
-... = ρ_S' ⊗ σ : by sorry,
+    calc _
+         ... = ∑ i, (p i) * Tr₁ ((V ⊗ 1) ρ_SS' (V ⊗ 1)†) ⊗ |i⟩⟨i| : by sorry
+         ... = ∑ i, (p i) * ρ_S' ⊗ |i⟩⟨i| : by sorry
+         ... = _ : by sorry,
 end
