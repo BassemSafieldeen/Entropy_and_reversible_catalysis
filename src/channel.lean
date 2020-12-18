b31f15f471babca1fc34b75bbc6e84bdca969fec
javipus <javier.prietop@estudiante.uam.es>
2020-12-18 15:31:42 +0100

    Added unitary evolution

--- src/channel.lean
+++ src/channel.lean
@@ -1,8 +1,17 @@
+import to_mathlib_maybe.Hilbert_space
 import state
 
-variables 
+variables
+{ℋ : Type} [complex_hilbert_space ℋ]
 {ℋ₁ : Type} [complex_hilbert_space ℋ₁]
 {ℋ₂ : Type} [complex_hilbert_space ℋ₂]
+{U : module.End ℂ ℋ} [unitary U]
+{ρ : module.End ℂ ℋ} [quantum_state ρ]
+
+/--
+Unitary evolution of a quantum state
+-/
+def evolve (U : module.End ℂ ℋ) (ρ : module.End ℂ ℋ) : module.End ℂ ℋ := U ∘ ρ ∘ U†
 
 /--
 A quantum channel is a linear map between linear operators 
@@ -13,9 +22,9 @@ class quantum_channel (𝒩 : (module.End ℂ ℋ₁) →ₗ[ℂ] (module.End �
 
 variables
 {𝒩 : (module.End ℂ ℋ₁) →ₗ[ℂ] (module.End ℂ ℋ₂)} [quantum_channel 𝒩]
-{ρ σ : (ℋ₁ →ₗ[ℂ] ℋ₁)} [quantum_state ρ] [quantum_state σ]
+{σ σ' : (ℋ₁ →ₗ[ℂ] ℋ₁)} [quantum_state ρ] [quantum_state σ]
 
-example : 𝒩(ρ + σ) = 𝒩(ρ) + 𝒩(σ) := 
+example : 𝒩(σ + σ') = 𝒩(σ) + 𝒩(σ') := 
 begin
     rw linear_map.map_add,
 end
