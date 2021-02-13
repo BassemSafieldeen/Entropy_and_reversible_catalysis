5d0466675d7eda0f1a73d628c8ea63a2ec0577b2
BassemSafieldeen <bassemsafieldeen@gmail.com>
2021-02-13 16:09:13 +0100

    Small changes

--- src/README.lean
+++ src/README.lean
@@ -1,16 +1,21 @@
 import linear_algebra.eigenspace
 
 import to_mathlib_maybe.Hilbert_space
+import to_mathlib_maybe.pTrace
 
 import to_qShannon_theory_maybe.state
 import to_qShannon_theory_maybe.entropy
 
 open_locale tensor_product big_operators
 
+notation `Trₑ ` ρ := bipartite.Tr₂ ρ
+
 variables
 {ℋₛ : Type} [complex_hilbert_space ℋₛ]
 {ℋₑ : Type} [complex_hilbert_space ℋₑ]
 {ℋₛₑ : Type} [complex_hilbert_space ℋₛₑ]
+-- talk about relation between ℋₛₑ and the other two
+-- to make progress with the error below (no guarantees).
 
 /-
 The people said: Hey, suppose you have two quantum states.
@@ -34,14 +39,14 @@ example :
 some_condition ρₛ ρₛ'
 ↔
 ∃ (U : module.End ℂ ℋₛₑ) [unitary U], 
-∃ (σₑ : module.End ℂ ℋₑ) [quantum_state σₑ]
-Trₑ (U ∘ ρₛ ⊗ σₑ ∘ U†) = ρₛ' ⊗ σₑ := 
+∃ (σₑ : module.End ℂ ℋₑ) [quantum_state σₑ],
+Trₑ (U ∘ (ρₛ ⊗ σₑ) ∘ U†) = ρₛ' ⊗ σₑ := 
 begin
   sorry
 end
 
 /-
-The paper says:
+The paper says: I can prove something close.
 -/
 
 def condition := H(ρₛ) ≤ H(ρₛ') 
@@ -52,9 +57,9 @@ condition ρₛ ρₛ'
 ∀ ε > 0,
 ∃ (U : module.End ℂ ℋₛₑ) [unitary U], 
 ∃ (σₑ : module.End ℂ ℋₑ) [quantum_state σₑ],
-Trₑ (U ∘ ρₛ ⊗ σₑ ∘ U†) = σₑ 
+Trₑ (U ∘ (ρₛ ⊗ σₑ) ∘ U†) = σₑ 
 ∧
-D(Trₑ(U ∘ ρₛ ⊗ σₑ ∘ U†), ρₛ') ≤ ε := 
+D(Trₑ (U ∘ (ρₛ ⊗ σₑ) ∘ U†), ρₛ') ≤ ε := 
 begin
   sorry
 end
